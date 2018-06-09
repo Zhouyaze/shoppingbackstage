cacaa12af3c37e377ba4515f2c51fd06391926fd
yaze <a1873766272>
2018-06-09 17:45:40 +0800

    2018-6-9 zyz PM 17:45

--- src/main/java/com/zhkj/shopmall/shoppingbackstage/shopping_backstage_dao/mapper/shopping_backstage_Commodity/UpdateCommodityMapper.java
+++ src/main/java/com/zhkj/shopmall/shoppingbackstage/shopping_backstage_dao/mapper/shopping_backstage_Commodity/UpdateCommodityMapper.java
@@ -1,9 +1,12 @@
 package com.zhkj.shopmall.shoppingbackstage.shopping_backstage_dao.mapper.shopping_backstage_Commodity;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface UpdateCommodityMapper {
 
-    int UpdateCommodity();
-    int UpdateCommodityPrice();
-    int UpdateCommodityPicture();
+    int UpdateCommodity(Integer id);
+    int UpdateCommodityPrice(Integer id);
+    int UpdateCommodityPicture( Integer id);
 
 }
