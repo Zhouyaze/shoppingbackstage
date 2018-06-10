cc12f165367f5470254d9105bcd00c5d86282a1e
yaze <a1873766272>
2018-06-10 15:31:32 +0800

    2018-6-9 zyz PM 15:31

--- src/main/java/com/zhkj/shopmall/shoppingbackstage/shopping_backstage_controller/advertisementcontroller/AdvertiseMentController.java
+++ src/main/java/com/zhkj/shopmall/shoppingbackstage/shopping_backstage_controller/advertisementcontroller/AdvertiseMentController.java
@@ -3,21 +3,39 @@ package com.zhkj.shopmall.shoppingbackstage.shopping_backstage_controller.advert
 import com.zhkj.shopmall.shoppingbackstage.shopping_backstage_dao.entity.AdvertisementEntity;
 import com.zhkj.shopmall.shoppingbackstage.shopping_backstage_service.impl.shopping_backstage_advertisement.service.Advertisement_IMPL;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpSession;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
 
 @RestController
 public class AdvertiseMentController {
     @Autowired
     Advertisement_IMPL advertisement_impl;
+    @InitBinder
+    protected void initBinder(WebDataBinder binder) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
+    }
     @RequestMapping("/addAdvertise")
-    public String insertAdvertise(@ModelAttribute AdvertisementEntity advertisementEntity) {
+    public String insertAdvertise(@ModelAttribute AdvertisementEntity advertisementEntity, MultipartFile file, HttpSession session) throws IOException {
         String msg = null;
+        if(upload(file,session)>0){
+            String url= (String) session.getAttribute("FileUrl");
+            advertisementEntity.setPictureUrl(url);
         if (advertisement_impl.addAdvertise(advertisementEntity) > 0) {
             msg = "添加广告成功";
         } else {
             msg = "添加广告失败";
+        }}else {
+            msg="上传失败";
         }
         return msg;
     }
@@ -54,9 +72,27 @@ public class AdvertiseMentController {
         }
         return msg;
     }
-
-    @RequestMapping("/selectAll")
-    public Object selectAdvertise(@ModelAttribute AdvertisementEntity advertisementEntity, Integer currentPage) {
-        return advertisement_impl.selectAll(advertisementEntity, currentPage);
+    //@RequestMapping("/upload")
+    public int upload( @RequestParam(name = "file")MultipartFile file, HttpSession session) throws IOException {
+        if (!file.isEmpty()){
+            String saveFileName = file.getOriginalFilename();//文件名
+            Random random=new Random(99999999);
+            int num=random.nextInt();
+            Date date=new Date();
+            SimpleDateFormat sf = new SimpleDateFormat("yyyyMMdd");
+            String xiangmuPath = ClassLoader.getSystemClassLoader().getResource("").getPath();
+            String bigXiangmuPath = xiangmuPath.substring(0,xiangmuPath.indexOf("out")) + "img/bigImgPath";
+            // 创建File对象 为存储图片做准备
+            File fi = new File(bigXiangmuPath);
+            if(!fi.exists()){
+                fi.mkdirs();
+            }
+            String fileName=fi+"/"+sf.format(date)+num+saveFileName;//少个路径
+            file.transferTo(new File(fileName));
+            session.setAttribute("FileUrl",fileName);
+            return 1;
+        }else {
+            return 0;
+        }
     }
 }
---
+++ src/main/java/com/zhkj/shopmall/shoppingbackstage/shopping_backstage_controller/advertisementcontroller/SelectController.java
@@ -0,0 +1,29 @@
+package com.zhkj.shopmall.shoppingbackstage.shopping_backstage_controller.advertisementcontroller;
+
+import com.zhkj.shopmall.shoppingbackstage.shopping_backstage_dao.entity.AdvertisementEntity;
+import com.zhkj.shopmall.shoppingbackstage.shopping_backstage_service.impl.shopping_backstage_advertisement.service.Advertisement_IMPL;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@RestController
+public class SelectController {
+    @Autowired
+    Advertisement_IMPL advertisement_impl;
+    @InitBinder
+    protected void initBinder(WebDataBinder binder) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
+    }
+    @RequestMapping("/selectAll")
+    public Object selectAdvertise(@ModelAttribute AdvertisementEntity advertisementEntity, Integer currentPage) {
+        return advertisement_impl.selectAll(advertisementEntity, currentPage);
+    }
+}
