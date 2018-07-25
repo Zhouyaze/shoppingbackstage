42d3f75129c5901f58fb0f87e67e933eb92b8b6a
yaze <a1873766272>
2018-07-25 10:12:49 +0800

    2018-7-25 zyz PM 10:12

--- src/main/java/com/zhkj/shopmall/shoppingbackstage/shopping_backstage_controller/ReturnedPurchaseController/ReturnedPurchaseController.java
+++ src/main/java/com/zhkj/shopmall/shoppingbackstage/shopping_backstage_controller/ReturnedPurchaseController/ReturnedPurchaseController.java
@@ -13,44 +13,47 @@ import java.util.List;
 @RestController
 public class ReturnedPurchaseController {
 
-
-//    @RequestMapping(value = "/sendMessage",method = RequestMethod.GET)
-//    public Result sendMessage(String json){
-//        ReturnedPurchaseVO returnedPurchaseVO = JSON.parseObject(json,ReturnedPurchaseVO.class);
-//
-//        return Result;
-//    }
-
-    /**
-     * 前台传过退货   进货信息   并添加数据到库
-     */
     @Autowired
     ReturnedPurchaseService returnedPurchaseService;
 
-
-    @RequestMapping("selectOne")
+    /**
+     * 前台查询 退货 发货 展示
+     * @param returnedPurchaseEntity
+     * @return
+     */
+    @RequestMapping("selectType")
     public List<ReturnedPurchaseEntity> select(ReturnedPurchaseEntity returnedPurchaseEntity){
-        return returnedPurchaseService.selectType(returnedPurchaseEntity);
+          return returnedPurchaseService.selectNews(returnedPurchaseEntity);
     }
 
+    /**
+     * 前台 退货  下订单数据
+     * @return 文杰
+     */
     @RequestMapping("saveReturned")
     public String saveReturn(){
         String json="{\"messageType\":\"2\",\"manifest\":\"1234567890\",\"returnUserName\":\"狗浩宇\",\"returnUserAddress\":\"河南省洛阳市涧西区几安南街33号3单元2号楼202\",\"commodityName\":\"苹果\",\"count\":\"9\",\"specification1\":\"褐色\",\"specification2\":\"xxxl\",\"specification3\":\"\",\"specification4\":\"\"}";
         ReturnedPurchaseVO returnedPurchaseVO = JSON.parseObject(json,ReturnedPurchaseVO.class);
-
-        return returnedPurchaseService.saveReturned(returnedPurchaseVO);
+        return returnedPurchaseService.saveNews(returnedPurchaseVO);
     }
 
 
-   // @RequestMapping("sendJosn")
+    /**
+     * 前台查询 发送 进销存  逐条发送
+     * @param returnedPurchaseEntity
+     * @return 国超
+     */
+    @RequestMapping("sendJosn")
+    public ReturnedPurchaseEntity sendJson1(ReturnedPurchaseEntity returnedPurchaseEntity){
+
+        return returnedPurchaseService.sendNews(returnedPurchaseEntity);
+    }
+
+    @RequestMapping("sendJosn1")
     public String sendJson(){
-        String json="{\"messageType\":\"3\",\"operatingStatus\":\"4\",\"manifest\":\"123456\",\"returnUserName\":\"崔文杰\",\"returnUserAddress\":\"河南省洛阳市涧西区几安南街33号3单元2号楼202\",\"commodityName\":\"苹果\",\"count\":\"9\",\"specification1\":\"褐色\",\"specification2\":\"xxxl\",\"specification3\":\"\",\"specification4\":\"\"}";
+        String json="{\"messageType\":\"2\",\"operatingStatus\":\"4\",\"manifest\":\"123456\",\"returnUserName\":\"崔文杰\",\"returnUserAddress\":\"河南省洛阳市涧西区几安南街33号3单元2号楼202\",\"commodityName\":\"苹果\",\"count\":\"9\",\"specification1\":\"褐色\",\"specification2\":\"xxxl\",\"specification3\":\"\",\"specification4\":\"\"}";
         ReturnedPurchaseVO returnedPurchaseVO = JSON.parseObject(json,ReturnedPurchaseVO.class);
         return returnedPurchaseService.querReturned(returnedPurchaseVO);
     }
 
-//    @RequestMapping("sendJosn")
-//    public String sendJson1(ReturnedPurchaseEntity returnedPurchaseEntity){
-//       // return returnedPurchaseService.querReturn(returnedPurchaseEntity);
-//    }
 }
--- src/main/java/com/zhkj/shopmall/shoppingbackstage/shopping_backstage_service/mapper/shopping_backage_ReturnedService/ReturnedPurchaseService.java
+++ src/main/java/com/zhkj/shopmall/shoppingbackstage/shopping_backstage_service/mapper/shopping_backage_ReturnedService/ReturnedPurchaseService.java
@@ -9,26 +9,31 @@ import java.util.List;
 public interface ReturnedPurchaseService {
 
     /**
-     * 添加退货商品信息
+     * 添加退货发货商品信息到消息表
+     * @param returnedPurchaseVO
+     * @return 文杰
+     */
+    String saveNews(ReturnedPurchaseVO returnedPurchaseVO);
+
+
+    /**
+     * 查询退货 发货商品  发送到进销存 逐条发送
+     * @return 国超
+     */
+    ReturnedPurchaseEntity sendNews(@Param("returned") ReturnedPurchaseEntity returnedPurchaseEntity);
+
+
+    /**
+     * 查询 退货  发货商品 前台展示
+     * @param returnedPurchaseEntity
+     * @return
+     */
+    List<ReturnedPurchaseEntity> selectNews(ReturnedPurchaseEntity returnedPurchaseEntity);
+
+    /**
+     * 测试 国超 返回数据
      * @param returnedPurchaseVO
      * @return
      */
-    String saveReturned(ReturnedPurchaseVO returnedPurchaseVO);
-
-
-    /**
-     * 查询退货商品   发送到进销存
-     * @return
-     */
     String querReturned(ReturnedPurchaseVO returnedPurchaseVO);
-
-
-    /**
-     * 查询退货商品   发送到进销存
-     * @return
-     */
-    int querReturn(@Param("returned") ReturnedPurchaseEntity returnedPurchaseEntity);
-
-
-    List<ReturnedPurchaseEntity> selectType(ReturnedPurchaseEntity returnedPurchaseEntity);
 }
